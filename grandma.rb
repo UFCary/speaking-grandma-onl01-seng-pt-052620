12d5d9f580ef0d4e0289f799ed7bc2a0b221f16a
Cary Williams <cary.l.williams00@gmail.com>
2020-05-28 22:46:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   "I LOVE YOU TOO PUMPKIN!"
 if phrase.upcase == "I LOVE YOU GRANDMA!"
-  
+  "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase && 
   "I LOVE YOU TOO PUMPKIN!"
 else
